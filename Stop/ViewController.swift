4f3157d5814f6290601463e0da413e5075204b75
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-04-10 15:34:27 +0900

    S

--- Stop/ViewController.swift
+++ Stop/ViewController.swift
@@ -3,23 +3,43 @@
 //  Stop
 //
 //  Created by D7703_25 on 2018. 4. 10..
-//  Copyright © 2018년 김종현. All rights reserved.
+//  Copyright © 2018년 김재현. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Lab: UILabel!
+    var count = 0
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateTime() {
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        Lab.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+        
     }
 
-
+    @IBAction func BtS(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()
+            
+        })
+    }
+    
+    @IBAction func BtT(_ sender: Any) {
+         myTimer.invalidate()
+    }
+    @IBAction func BtR(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+        Lab.text = "00:00:00"
+    }
 }
 
